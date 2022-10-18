fixed issues in gradient computation

ngp_nerf/encoding.py
@@ -14,8 +14,7 @@ class MultiLevelHashEncoding(torch.nn.Module):
     This implementation uses a global encoding matrix and equal number
     of encoding vectors for each resolution level. The module employs the
     proposed hashing method to identify the indices of (potentially shared)
-    encoding vectors for each grid vertex (hashing is used for resolutions that
-    could use a direct mapping too). The grids (called levelmaps) are
+    encoding vectors for each grid vertex. The grids (called levelmaps) are
     treated as images/volumes and can be pre-computed. The query
     locations are reshaped into a grid format compatible with `F.grid_sample`
     to interpolate the encodings.
@@ -58,26 +57,44 @@ class MultiLevelHashEncoding(torch.nn.Module):
         self.min_res = min_res
         self.max_res = max_res
 
-        resolutions = torch.ceil(
-            torch.exp(
-                torch.linspace(
-                    torch.log(torch.tensor(self.min_res)),
-                    torch.log(torch.tensor(self.max_res)),
-                    self.n_levels,
-                )
-            )
-        ).int()
         with torch.no_grad():
-            for level, res in enumerate(resolutions):
-                # For each resolution R, we form a (E,R,R,R) levelmap whose vertices
-                # contain a view of the global embedding vectors in first dimension.
-                # The encoding vector indices are computed by hashing the spatial
-                # grid location. We precompute the encoding vector indices here.
-                res_range = torch.arange(res)
-                index_coords = torch.stack(
-                    torch.meshgrid([res_range] * self.n_input_dims, indexing="ij"), -1
+            # Equation 2 and 3 in the paper to determine the number of grid vertices
+            # per resolution level
+            growth_factor = torch.exp(
+                (
+                    torch.log(torch.tensor(self.max_res))
+                    - torch.log(torch.tensor(self.min_res))
                 )
-                indices = hashing.xor_index_hashing(index_coords, self.n_encodings)
+                / (self.n_levels - 1)
+            )
+            resolutions = torch.floor(
+                torch.tensor(
+                    [
+                        self.min_res * growth_factor**level
+                        for level in range(self.n_levels)
+                    ]
+                )
+            ).long()
+            print(resolutions)
+            # For each resolution R, we form a (E,R,R,R) levelmap whose vertices
+            # contain a view of the global embedding vectors in first dimension.
+            # The encoding vector indices are computed by hashing the spatial
+            # grid location. We precompute the encoding vector indices here.
+            for level, res in enumerate(resolutions):
+                index_coords = torch.stack(
+                    torch.meshgrid(
+                        [torch.arange(res)] * self.n_input_dims, indexing="ij"
+                    ),
+                    -1,
+                )
+                if res**self.n_input_dims < self.n_encodings:
+                    res_shape = [res] * self.n_input_dims
+                    indices = hashing.ravel_index(
+                        index_coords, res_shape, self.n_encodings
+                    )
+                    print(len(torch.unique(indices)))
+                else:
+                    indices = hashing.xor_index_hashing(index_coords, self.n_encodings)
                 self.register_buffer("level_embedding_indices" + str(level), indices)
 
     def forward(self, x):

ngp_nerf/tests/test_encoding.py
@@ -4,7 +4,7 @@ from ngp_nerf.encoding import MultiLevelHashEncoding
 
 def test_encoding_output_shape_3d():
     mlh = MultiLevelHashEncoding(
-        n_encodings=2**14,
+        n_encodings=2**16,
         n_input_dims=3,
         n_embed_dims=2,
         n_levels=4,
@@ -40,14 +40,18 @@ def test_encoding_gradients():
         max_res=64,
     )
     # Set all embeddings to one
-    mlh.embmatrix.data.copy_(torch.ones_like(mlh.embmatrix) * 2)
+    mlh.embmatrix.data.copy_(torch.ones_like(mlh.embmatrix) * 1)
 
-    f = mlh(torch.tensor([[0.125, 0.125]]))
+    f = mlh(torch.tensor([[0.31231, 0.7312]]))
     loss = f.square().sum()
     loss.backward()
     # If there isn't a collison, we should get 4 embedding vectors to receive
-    # gradient of 1.0 per level
-    assert (mlh.embmatrix.grad[..., 0] > 0.0).sum().int() == 4
-    assert (mlh.embmatrix.grad[..., 1] > 0.0).sum().int() == 4
-    assert (mlh.embmatrix.grad[..., 2] > 0.0).sum().int() == 4
-    assert (mlh.embmatrix.grad[..., 3] > 0.0).sum().int() == 4
+    # gradient (scaled by the weights of the bilinear interpolation). Note, the
+    # coordinate above is chosen, so that on no level it falls exactly one a grid
+    # point
+    # mask = mlh.embmatrix.grad[..., 1] != 0.0
+    # print(mlh.embmatrix.grad[..., 1][mask])
+    assert (mlh.embmatrix.grad[..., 0] != 0.0).sum().int() == 4
+    assert (mlh.embmatrix.grad[..., 1] != 0.0).sum().int() == 4
+    assert (mlh.embmatrix.grad[..., 2] != 0.0).sum().int() == 4
+    assert (mlh.embmatrix.grad[..., 3] != 0.0).sum().int() == 4

scripts/visualize_indexing.py
@@ -5,14 +5,14 @@ from ngp_nerf import hashing
 
 
 def main():
-    T = 512
-    res_range = torch.arange(128)
+    T = 4096
+    res_range = torch.arange(32)
     index_coords = torch.stack(
         torch.meshgrid([res_range] * 2, indexing="ij"), -1
     ).long()
 
     # uniform distribution of hash values desired
-    indices = hashing.xor_index_hashing(index_coords, T)
+    indices = hashing.ravel_index(index_coords, (32, 32), T)
     plt.bar(*np.unique(indices, return_counts=True))
     plt.show()
 