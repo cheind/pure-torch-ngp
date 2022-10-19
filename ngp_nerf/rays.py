d1fd59f80a3e4d76e5c8409a6d90942233f82a60
Christoph Heindl <christoph.heindl@gmail.com>
2022-10-19 20:17:59 +0200

    cleanup

--- ngp_nerf/rays.py
+++ ngp_nerf/rays.py
@@ -31,14 +31,3 @@ def intersect_ray_aabb(
     tnear = t1.max(dim=1)[0]
     tfar = t2.min(dim=1)[0]
     return tnear, tfar
-
-
-# vec2 intersectAABB(vec3 rayOrigin, vec3 rayDir, vec3 boxMin, vec3 boxMax) {
-#     vec3 tMin = (boxMin - rayOrigin) / rayDir;
-#     vec3 tMax = (boxMax - rayOrigin) / rayDir;
-#     vec3 t1 = min(tMin, tMax);
-#     vec3 t2 = max(tMin, tMax);
-#     float tNear = max(max(t1.x, t1.y), t1.z);
-#     float tFar = min(min(t2.x, t2.y), t2.z);
-#     return vec2(tNear, tFar);
-# };
