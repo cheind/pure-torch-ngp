05152f98895fcf22befeca11914ed949730d3a87
Christoph Heindl <cheind@profactor.at>
2022-10-21 12:52:22 +0200

    mixed precision and more

--- ngp_nerf/apps/nerf.py
+++ ngp_nerf/apps/nerf.py
@@ -1,9 +1,10 @@
-import enum
+import math
 import torch
 import torch.nn
 import torch.nn.functional as F
 import torch.utils.data
 import torch.optim
+import time
 
 from ngp_nerf import rays, radiance
 
@@ -77,12 +78,13 @@ class NeRF(torch.nn.Module):
 
 def train(
     nerf: torch.nn.Module,
-    scene: MultiViewScene,
+    train_scene: MultiViewScene,
+    test_scene: MultiViewScene,
     dev: torch.device,
     batch_size: int,
-    n_epochs: int,
     n_samples: int,
     lr: float = 1e-2,
+    max_train_secs: float = 180,
 ):
 
     opt = torch.optim.AdamW(
@@ -96,8 +98,13 @@ def train(
         lr=lr,
     )
 
-    views = list(range(len(scene.views)))
-    train_ds = MultiViewDataset(scene=scene, view_indices=views[1:])
+    sched = torch.optim.lr_scheduler.ReduceLROnPlateau(
+        opt, mode="min", factor=0.75, patience=40, min_lr=1e-6
+    )
+
+    scaler = torch.cuda.amp.GradScaler()
+
+    train_ds = MultiViewDataset(scene=train_scene)
     train_dl = torch.utils.data.DataLoader(
         train_ds,
         batch_size=10,
@@ -111,9 +118,14 @@ def train(
 
     pbar_postfix = {"loss": 0.0}
     step = 0
-    for epoch in range(n_epochs):
+    t_start = time.time()
+    t_last_dump = time.time()
+    for _ in range(1000):
         pbar = tqdm(train_dl, mininterval=0.1)
         for batch in pbar:
+            t_now = time.time()
+            opt.zero_grad()
+
             # Collapse mini-batch dim and upload
             batch = [b.view(-1, *b.shape[2:]) for b in batch]
             batch = [b.to(dev) for b in batch]
@@ -126,51 +138,91 @@ def train(
             # model does not have to learn randomness.
             color = color * alpha[..., None] + noise * (1 - alpha[..., None])
 
-            ts = rays.sample_rays_uniformly(tnear, tfar, n_samples)
-            xyz = o[:, None] + ts[..., None] * d[:, None]  # (B,T,3)
-            nxyz = rays.normalize_xyz_in_aabb(xyz, aabb_min, aabb_max)
+            with torch.cuda.amp.autocast():
+                ts = rays.sample_rays_uniformly(tnear, tfar, n_samples)
+                xyz = o[:, None] + ts[..., None] * d[:, None]  # (B,T,3)
+                nxyz = rays.normalize_xyz_in_aabb(xyz, aabb_min, aabb_max)
 
-            pred_sample_sigma, pred_sample_color = nerf(nxyz.view(-1, 3))
-            pred_colors, pred_transm, _ = radiance.integrate_path(
-                pred_sample_color.view(-1, n_samples, 3),
-                pred_sample_sigma.view(-1, n_samples),
-                ts,
-                tfar,
-            )
-            pred_alpha = 1.0 - pred_transm[:, -1]
-            pred_colors = pred_colors * pred_alpha[..., None] + noise * (
-                1 - pred_alpha[..., None]
-            )
+                pred_sample_sigma, pred_sample_color = nerf(nxyz.view(-1, 3))
+                pred_colors, pred_transm, _ = radiance.integrate_path(
+                    pred_sample_color.view(-1, n_samples, 3),
+                    pred_sample_sigma.view(-1, n_samples),
+                    ts,
+                    tfar,
+                )
+                pred_alpha = 1.0 - pred_transm[:, -1]
+                pred_colors = pred_colors * pred_alpha[..., None] + noise * (
+                    1 - pred_alpha[..., None]
+                )
 
-            opt.zero_grad()
-            loss = F.mse_loss(pred_colors, color)
+                loss = F.mse_loss(pred_colors, color)
+                if t_now - t_last_dump > 30:
+                    render_test_scenes(
+                        nerf,
+                        test_scene,
+                        dev,
+                        batch_size,
+                        n_samples * 4,
+                        t_now - t_start,
+                        show=False,
+                    )
+                    t_last_dump = t_now
+
+            loss = scaler.scale(loss)
             loss.backward()
-            opt.step()
-            # sched.step()
+            scaler.step(opt)
+            scaler.update()
+            # opt.step()
+            sched.step(loss)
             pbar_postfix["loss"] = loss.item()
+            pbar_postfix["lr"] = sched._last_lr
             pbar.set_postfix(**pbar_postfix, refresh=False)
             step += 1
+            if (t_now - t_start) > max_train_secs:
+                return
 
-            if (step % 100) == 0:
-                v = scene.views[0].to(dev)
-                img = v.render_image(
-                    nerf,
-                    aabb_min,
-                    aabb_max,
-                    batch_size=batch_size,
-                    n_samples=100,
-                )
-                fig, ax = v.plot_image(img)
-                fig.savefig(f"tmp/nerf_{step:04d}.png")
-                import matplotlib.pyplot as plt
 
-                plt.show()
+@torch.no_grad()
+def render_test_scenes(
+    nerf: radiance.RadianceField,
+    test_scene: MultiViewScene,
+    dev: torch.device,
+    batch_size: int,
+    n_samples: int = 100,
+    t_elapsed: int = 0.0,
+    show: bool = False,
+):
+    import matplotlib.pyplot as plt
+
+    N = len(test_scene.views)
+    fig = plt.figure(constrained_layout=True, figsize=(10, 10))
+    ncols = 2
+    nrows = int(math.ceil(N / ncols))
+    spec = fig.add_gridspec(ncols=ncols, nrows=nrows)
+
+    for vidx, v in enumerate(test_scene.views):
+        ax = fig.add_subplot(spec[vidx // ncols, vidx % ncols])
+        v = v.to(dev)
+        img = v.render_image(
+            nerf,
+            test_scene.aabb_minc.to(dev),
+            test_scene.aabb_maxc.to(dev),
+            batch_size=batch_size,
+            n_samples=n_samples,
+        )
+        v.plot_image(img, ax=ax)
+    fig.savefig(f"tmp/nerf_secs{int(t_elapsed):03d}.png")
+    if show:
+        plt.show()
+    else:
+        plt.close(fig)
 
 
 if __name__ == "__main__":
     dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     scene = MultiViewScene()
-    scene.load_from_json("data/suzanne/transforms.json")
+    scene.load_from_json("data/fox/transforms.json")
+    # scene.render_world()
     nerf = NeRF(
         3,
         n_hidden=64,
@@ -180,4 +232,36 @@ if __name__ == "__main__":
         max_res=256,
         is_hdr=False,
     ).to(dev)
-    train(nerf, scene, dev, batch_size=2**14, n_epochs=20, n_samples=100, lr=1e-3)
+
+    train_scene, test_scene = scene.split(0.85)
+
+    for v in test_scene.views:
+        v.K[0, 0] /= 2
+        v.K[1, 1] /= 2
+        v.K[0, 2] = 512 / 2
+        v.K[1, 2] = 512 / 2
+        v.image_spatial_shape = (512, 512)
+
+    # train_scene.render_world()
+    # test_scene.render_world()
+
+    train_time = 60 * 3
+    train(
+        nerf,
+        train_scene,
+        test_scene,
+        dev,
+        batch_size=2**16,
+        n_samples=40,
+        lr=1e-2,
+        max_train_secs=train_time,
+    )
+    render_test_scenes(
+        nerf,
+        test_scene,
+        dev,
+        batch_size=2**16,
+        n_samples=100,
+        t_elapsed=train_time,
+        show=True,
+    )
