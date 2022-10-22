747ee7c94a2449761b4c52ed63e33399437ed643
Christoph Heindl <cheind@profactor.at>
2022-10-22 21:39:46 +0200

    added radiance export

---
+++ ngp_nerf/apps/nerf_export.py
@@ -0,0 +1,32 @@
+import torch
+import numpy as np
+
+from .nerf import NeRF
+from ngp_nerf import radiance
+
+
+if __name__ == "__main__":
+    dev = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    nerf_kwargs, state_dict = torch.load("tmp/nerf.pth")
+    nerf = NeRF(**nerf_kwargs)
+    nerf.load_state_dict(state_dict)
+    nerf = nerf.to(dev).eval()
+
+    with torch.no_grad():
+        d, rgb = radiance.rasterize_field(
+            nerf, (128, 128, 128), dev=dev, batch_size=2**16
+        )
+        rgbd = torch.cat((rgb, d.unsqueeze(-1)), -1).cpu()
+
+    from pathlib import Path
+    from PIL import Image
+
+    outdir = Path("tmp/nerf_slices/")
+    outdir.mkdir(exist_ok=True, parents=False)
+    for idx, slice in enumerate(rgbd):
+        slice = (slice * 255).to(torch.uint8).numpy()
+        img = Image.fromarray(slice, mode="RGBA")
+        img.save(outdir / f"slice_{idx:04d}.png")
+
+    rgbd = rgbd.cpu()
+    np.savez("tmp/volume.npz", rgb=rgbd[..., :3], d=rgbd[..., 3])
