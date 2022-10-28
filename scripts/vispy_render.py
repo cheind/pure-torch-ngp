3b753b39e0cf5cadd54dfb44b09e31a82cc3b015
Christoph Heindl <cheind@profactor.at>
2022-10-28 16:26:56 +0200

    cleanup

--- scripts/vispy_render.py
+++ scripts/vispy_render.py
@@ -6,48 +6,39 @@ from vispy import app, scene, io
 from vispy.color import get_colormaps, BaseColormap
 from vispy.visuals.transforms import STTransform
 
-# Read volume
-vol1 = np.load("tmp/volume.npz")["d"]
-
-
-print(vol1.min(), vol1.max())
-# vol2 = np.load(io.load_data_file("brain/mri.npz"))["data"]
-# vol2 = np.flipud(np.rollaxis(vol2, 1))
-
-# Prepare canvas
 canvas = scene.SceneCanvas(keys="interactive", size=(800, 600), show=True)
 canvas.measure_fps()
-
-# Set up a viewbox to display the image with interactive pan/zoom
 view = canvas.central_widget.add_view()
 
+
+# Read volume
+data = np.load("tmp/volume.npz")
+vol_density = data["d"]
+vol_aabb = data["aabb"]
+
+
 # Create the volume visuals, only one is visible
-volume1 = scene.visuals.Volume(
-    vol1,
+volume = scene.visuals.Volume(
+    vol=vol_density,
     parent=view.scene,
     threshold=0.0,
     method="translucent",
-    clim=(0.0, 1.0),
+    clim=(0.1, 2.0),
     interpolation="linear",
-    relative_step_size=0.1,
+    relative_step_size=0.2,
 )
-# volume1.transform = scene.STTransform(translate=(64, 64, 0))
-# volume2 = scene.visuals.Volume(vol2, parent=view.scene, threshold=0.2)
-# volume2.visible = False
 
-# Create three cameras (Fly, Turntable and Arcball)
+# volume.transform = scene.STTransform(translate=-(vol_aabb[1] - vol_aabb[0]) * 0.5)
+
 fov = 60.0
-cam1 = scene.cameras.FlyCamera(parent=view.scene, fov=fov, name="Fly")
-cam2 = scene.cameras.TurntableCamera(parent=view.scene, fov=fov, name="Turntable")
-cam3 = scene.cameras.ArcballCamera(parent=view.scene, fov=fov, name="Arcball")
-view.camera = cam2  # Select turntable at first
+cam = scene.cameras.TurntableCamera(parent=view.scene, fov=fov, name="Arcball")
+view.camera = cam  # Select turntable at first
+cam.reset()
 
-# Create an XYZAxis visual
 axis = scene.visuals.XYZAxis(parent=view)
 s = STTransform(translate=(50, 50), scale=(50, 50, 50, 1))
 affine = s.as_matrix()
 axis.transform = affine
-
 
 # create colormaps that work well for translucent and additive volume rendering
 class TransFire(BaseColormap):
@@ -72,66 +63,53 @@ translucent_cmaps = cycle([TransGrays(), TransFire()])
 opaque_cmap = next(opaque_cmaps)
 translucent_cmap = next(translucent_cmaps)
 
-interp_methods = cycle(volume1.interpolation_methods)
-interp = next(interp_methods)
+# interp_methods = cycle(volume1.interpolation_methods)
+# interp = next(interp_methods)
+# print(volume1.interpolation_methods)
 
-volume1.cmap = translucent_cmap
-
-# Implement axis connection with cam2
-@canvas.events.mouse_move.connect
-def on_mouse_move(event):
-    if event.button == 1 and event.is_dragging:
-        axis.transform.reset()
-
-        axis.transform.rotate(cam2.roll, (0, 0, 1))
-        axis.transform.rotate(cam2.elevation, (1, 0, 0))
-        axis.transform.rotate(cam2.azimuth, (0, 1, 0))
-
-        axis.transform.scale((50, 50, 0.001))
-        axis.transform.translate((50.0, 50.0))
-        axis.update()
+volume.cmap = TransGrays()
 
 
 # Implement key presses
-@canvas.events.key_press.connect
-def on_key_press(event):
-    global opaque_cmap, translucent_cmap
-    if event.text == "1":
-        cam_toggle = {cam1: cam2, cam2: cam3, cam3: cam1}
-        view.camera = cam_toggle.get(view.camera, cam2)
-        print(view.camera.name + " camera")
-        if view.camera is cam2:
-            axis.visible = True
-        else:
-            axis.visible = False
-    elif event.text == "2":
-        methods = ["mip", "translucent", "iso", "additive"]
-        method = methods[(methods.index(volume1.method) + 1) % 4]
-        print("Volume render method: %s" % method)
-        cmap = opaque_cmap if method in ["mip", "iso"] else translucent_cmap
-        volume1.method = method
-        volume1.cmap = cmap
-    elif event.text == "3":
-        volume1.visible = not volume1.visible
-    elif event.text == "4":
-        if volume1.method in ["mip", "iso"]:
-            cmap = opaque_cmap = next(opaque_cmaps)
-        else:
-            print("here")
-            cmap = translucent_cmap = next(translucent_cmaps)
-        volume1.cmap = cmap
-    elif event.text == "5":
-        interp = next(interp_methods)
-        volume1.interpolation = interp
-        print(f"Interpolation method: {interp}")
-    elif event.text == "0":
-        cam1.set_range()
-        cam3.set_range()
-    elif event.text != "" and event.text in "[]":
-        s = -0.025 if event.text == "[" else 0.025
-        volume1.threshold += s
-        th = volume1.threshold
-        print("Isosurface threshold: %0.3f" % th)
+# @canvas.events.key_press.connect
+# def on_key_press(event):
+#     global opaque_cmap, translucent_cmap
+#     if event.text == "1":
+#         cam_toggle = {cam1: cam2, cam2: cam3, cam3: cam1}
+#         view.camera = cam_toggle.get(view.camera, cam2)
+#         print(view.camera.name + " camera")
+#         if view.camera is cam2:
+#             axis.visible = True
+#         else:
+#             axis.visible = False
+#     elif event.text == "2":
+#         methods = ["mip", "translucent", "iso", "additive"]
+#         method = methods[(methods.index(volume1.method) + 1) % 4]
+#         print("Volume render method: %s" % method)
+#         cmap = opaque_cmap if method in ["mip", "iso"] else translucent_cmap
+#         volume1.method = method
+#         volume1.cmap = cmap
+#     elif event.text == "3":
+#         volume1.visible = not volume1.visible
+#     elif event.text == "4":
+#         if volume1.method in ["mip", "iso"]:
+#             cmap = opaque_cmap = next(opaque_cmaps)
+#         else:
+#             print("here")
+#             cmap = translucent_cmap = next(translucent_cmaps)
+#         volume1.cmap = cmap
+#     elif event.text == "5":
+#         interp = next(interp_methods)
+#         volume1.interpolation = interp
+#         print(f"Interpolation method: {interp}")
+#     elif event.text == "0":
+#         cam1.set_range()
+#         cam3.set_range()
+#     elif event.text != "" and event.text in "[]":
+#         s = -0.025 if event.text == "[" else 0.025
+#         volume1.threshold += s
+#         th = volume1.threshold
+#         print("Isosurface threshold: %0.3f" % th)
 
 
 # for testing performance
