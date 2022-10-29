b6f22dff25845b01e04b0c59b94f3b6e356b3b6c
Christoph Heindl <christoph.heindl@gmail.com>
2022-10-29 20:54:29 +0200

    added script to show how pathtracing works

--- scripts/visualize_path_tracing.py
+++ scripts/visualize_path_tracing.py
@@ -5,52 +5,53 @@ import matplotlib.pyplot as plt
 from ngptorch import radiance, geometric, sampling
 
 
-def main():
+def plot_density_scale(ds, show=True):
     o = torch.tensor([[-1.0, 0.0, 0.0]])
     d = torch.tensor([[1.0, 0.0, 0.0]])
 
     plane_n = torch.tensor([1.0, 0.0, 0.0])
-    plane_o = torch.tensor([0.2, 0.0, 0.0])
+    plane_o = torch.tensor([0.5, 0.0, 0.0])
 
     tnear = torch.tensor([[1.0]])
     tfar = torch.tensor([[2.0]])
 
-    ts = sampling.sample_ray_step_stratified(tnear, tfar, 100)
+    ts = sampling.sample_ray_step_stratified(tnear, tfar, 50)
 
     # Estimate colors and density values at sample positions
     xyz = geometric.evaluate_ray(o, d, ts)  # (T,B,3)
-    color = torch.tensor(mpl.colormaps["hsv"](xyz[..., 0].numpy()))  # (T,B,4)
+    color = torch.tensor(mpl.colormaps["jet"](xyz[..., 0].numpy()))  # (T,B,4)
     density = (
         (xyz - plane_o[None, None, :]).unsqueeze(-2) @ plane_n[None, None, :, None]
     ).squeeze(-1)
     mask = density < 0
     density[mask] = 0.0
-    density[~mask] *= 100
-    print(density.shape)
+    density[~mask] *= ds
 
     final_colors, transmittance, alpha = radiance.integrate_path(
         color[..., :3], density, ts, tfar
     )
     print(final_colors)
-    plt.vlines(
+    fig, ax = plt.subplots(figsize=(6, 5))
+    fig.text(0.35, 0.9, f"density scale factor {ds:.2f}")
+    ax.vlines(
         plane_o[0], 0.0, 1.0, colors="k", linestyles="--", zorder=3, label="plane"
     )
-    plt.plot(
+    ax.plot(
         ts[:, 0] - tnear[0, 0], transmittance[:, 0], c="gray", label="transmittance"
     )
-    plt.plot(
+    ax.plot(
         ts[:, 0] - tnear[0, 0],
         density[:, 0] / density.max(),
         c="g",
         label="relative density",
     )
 
-    plt.imshow(
+    ax.imshow(
         final_colors.view(1, 1, 3),
         extent=(ts[0, 0, 0] - tnear[0, 0], ts[0, 0, 0] - tnear[0, 0] + 0.1, 0.5, 0.6),
-        label="final color",
     )
-    plt.scatter(
+    plt.text(0.01, 0.45, "final color")
+    ax.scatter(
         ts[:, 0] - tnear[0, 0],
         transmittance[:, 0],
         c=color[:, 0],
@@ -58,12 +59,24 @@ def main():
         zorder=2,
         label="sampled colors",
     )
-    plt.xlabel("x")
-    plt.ylabel("transmittance")
-    plt.gca().set_aspect("equal")
+
+    ax.set_xlabel("x")
+    ax.set_ylabel("transmittance/density")
+    # ax.set_aspect("equal")
     plt.xlim(-0.1, 1.1)
     plt.ylim(0.0, 1.2)
+    plt.suptitle("Path tracing")
+
     plt.legend(loc="upper center", ncols=2)
+    plt.savefig(f"etc/path_tracing_{ds}.png", dpi=300)
+
+    return fig
+
+
+def main():
+    fig = plot_density_scale(10)
+    fig2 = plot_density_scale(100)
+
     plt.show()
 
 
