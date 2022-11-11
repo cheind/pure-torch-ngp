7c4d11a07c2a58d260a905ec9118c8f0b9492e1b
Christoph Heindl <cheind@profactor.at>
2022-11-11 10:08:43 +0100

    renamed parameters

--- torchngp/filtering.py
+++ torchngp/filtering.py
@@ -42,28 +42,28 @@ class OccupancyGridFilter(BoundsFilter):
         self,
         rf: RadianceField,
         res: int = 64,
-        update_interval: int = 16,
-        decay: float = 0.7,
         density_initial=0.02,
         density_threshold=0.01,
-        noise_scale: float = None,
-        selection_rate=0.25,
+        deterministic_mask: bool = True,
+        update_interval: int = 16,
+        update_decay: float = 0.7,
+        update_noise_scale: float = None,
+        update_selection_rate=0.25,
         dev: torch.device = None,
     ) -> None:
         super().__init__()
         self.rf = rf
         self.res = res
         self.update_interval = update_interval
-        self.decay = decay
+        self.update_decay = update_decay
         self.density_initial = density_initial
         self.density_threshold = density_threshold
-        self.selection_rate = selection_rate
-        if noise_scale is None:
-            noise_scale = 0.9
-        self.noise_scale = noise_scale
+        self.update_selection_rate = update_selection_rate
+        if update_noise_scale is None:
+            update_noise_scale = 0.9
+        self.update_noise_scale = update_noise_scale
         self.dev = dev
         self.grid = torch.full((res, res, res), density_initial, device=dev)
-        self.grid_mask = self.grid > density_threshold
 
     def test(self, xyz_ndc: torch.Tensor) -> torch.BoolTensor:
         mask = super().test(xyz_ndc)
@@ -71,23 +71,18 @@ class OccupancyGridFilter(BoundsFilter):
         ijk = (xyz_ndc + 1) * self.res * 0.5 - 0.5
         ijk = torch.round(ijk).clamp(0, self.res - 1).long()
 
-        ijk_valid = ijk[mask]
-        ijk_mask = self.grid_mask[ijk_valid[:, 2], ijk_valid[:, 1], ijk_valid[:, 0]]
-
-        out_mask = mask.clone()
-        out_mask[mask] &= ijk_mask
-
-        return out_mask
+        d_mask = self.grid[ijk[:, 2], ijk[:, 1], ijk[:, 0]] > self.density_threshold
+        return mask & d_mask
 
     @torch.no_grad()
     def update(self, global_step: int):
         if (global_step + 1) % self.update_interval > 0:
             return
 
-        self.grid *= self.decay
+        self.grid *= self.update_decay
 
-        if self.selection_rate < 1.0:
-            M = int(self.selection_rate * self.res**3)
+        if self.update_selection_rate < 1.0:
+            M = int(self.update_selection_rate * self.res**3)
             ijk = torch.randint(0, self.res, size=(M, 3), device=self.dev)
         else:
             ijk = geometric.make_grid(
@@ -98,7 +93,7 @@ class OccupancyGridFilter(BoundsFilter):
             ).view(-1, 3)
 
         noise = torch.rand_like(ijk, dtype=torch.float) - 0.5
-        noise *= self.noise_scale
+        noise *= self.update_noise_scale
         xyz = ijk + noise
         xyz_ndc = (xyz + 0.5) * 2 / self.res - 1.0
 
@@ -107,4 +102,3 @@ class OccupancyGridFilter(BoundsFilter):
         cur = self.grid[ijk[:, 2], ijk[:, 1], ijk[:, 0]]
         new = torch.maximum(d, cur)
         self.grid[ijk[:, 2], ijk[:, 1], ijk[:, 0]] = new
-        self.grid_mask = self.grid > self.density_threshold
