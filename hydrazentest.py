ba96ac1f191b94b27951c76b7eaf4d3d21ccc279
Christoph Heindl <cheind@profactor.at>
2022-11-18 07:27:53 +0100

    added poses

--- hydrazentest.py
+++ hydrazentest.py
@@ -1,134 +1,23 @@
 import torch
-from typing import Union
-import hydra
-from hydra.core.config_store import ConfigStore
 
-from hydra_zen import instantiate, make_config, make_custom_builds_fn, to_yaml
+from hydra_zen import instantiate, to_yaml
 
-
-builds = make_custom_builds_fn(populate_full_signature=True)
-
-Float3 = tuple[float, float, float]
-
-# Floats3x3 = Union[
-#     tuple[float, float, float, float, float, float, float, float, float],
-#     torch.FloatTensor,
-# ]
-# Floats3 = Union[tuple[float, float, float], torch.FloatTensor]
-# Floats2 = Union[tuple[float, float], torch.FloatTensor]
-
-
-# class Camera(torch.nn.Module):
-#     def __init__(self, focal_length: Floats2, rots: list[Floats3x3], ts: list[Floats3]):
-#         super().__init__()
-#         self.focal_length = torch.as_tensor(focal_length, dtype=torch.float32)
-#         self.R = torch.stack(rots)
-#         self.ts = torch.stack(ts)
-
-
-# class World(torch.nn.Module):
-#     def __init__(self, cams: list[Camera]):
-#         super().__init__()
-#         self.cams = torch.nn.ModuleList(cams)
-
-
-# CameraConf = builds(Camera)
-
-# WorldConf = builds(World)
-# worldcfg = WorldConf(
-#     cams=[
-#         CameraConf(
-#             focal_length=(1.0, 2.0),
-#             rots=[torch.eye(3).tolist()],
-#             ts=[torch.ones(3).tolist()],
-#         )
-#     ]
-# )
-# print(to_yaml(worldcfg))
-
-# world = instantiate(worldcfg)
-
-
-# class Camera(torch.nn.Module):
-#     def __init__(self, focal_length: float, size: tuple[int, int]) -> None:
-#         super().__init__()
-#         self.register_buffer("focal_length", torch.tensor([focal_length, focal_length]))
-#         self.register_buffer("size", torch.as_tensor(size))
-
-#     def extra_repr(self) -> str:
-#         out = ""
-#         out += f"focal_length={self.focal_length}, "
-#         out += f"size={self.size}"
-#         return out
-
-
-# class RadianceField(torch.nn.Module):
-#     def __init__(self, res: int = 64) -> None:
-#         super().__init__()
-#         self.res = res
-
-
-# class SpatialFilter(torch.nn.Module):
-#     def __init__(self, res: int = 32) -> None:
-#         super().__init__()
-#         self.res = res
-
-
-# class BoxTransform(torch.nn.Module):
-#     def __init__(
-#         self, minc: tuple[float, float, float], maxc: tuple[float, float, float]
-#     ) -> None:
-#         super().__init__()
-#         self.register_buffer("aabb", torch.as_tensor([minc, maxc]))
-
-
-# class Volume(torch.nn.Module):
-#     def __init__(
-#         self,
-#         rf: RadianceField,
-#         filter: SpatialFilter,
-#         boxt: BoxTransform,
-#     ) -> None:
-#         super().__init__()
-
-#         self.rf = rf
-#         self.filter = filter
-#         self.box_transform = boxt
-
-
-# class Scene(torch.nn.Module):
-#     def __init__(self, cams: dict[str, Camera], volume: Volume) -> None:
-#         super().__init__()
-#         self.cameras = torch.nn.ModuleDict(cams)
-#         self.volume = volume
-
-
-# CameraConf = builds(Camera, populate_full_signature=True)
-# RadianceFieldConf = builds(RadianceField, populate_full_signature=True)
-# SpatialFilterConf = builds(SpatialFilter, populate_full_signature=True)
-# BoxTransformConf = builds(BoxTransform, populate_full_signature=True)
-# VolConf = builds(
-#     Volume,
-#     rf=RadianceFieldConf(),
-#     filter=SpatialFilterConf(),
-#     boxt=BoxTransformConf((-1.0,) * 3, (1.0,) * 3),
-#     populate_full_signature=True,
-# )
-
-# SceneConfig = builds(
-#     Scene,
-#     cams={"cam_train": CameraConf(1.0, (100, 200))},
-#     volume=VolConf(),
-#     populate_full_signature=True,
-# )
-# print(to_yaml(SceneConfig))
 
 from hydra_zen import save_as_yaml, load_from_yaml
-from torchngp.conf import SceneConfig, CameraConf, Scene
+from torchngp.conf import SceneConfig, CameraConf, Scene, PosesConf, Pose
 
-scenecfg = SceneConfig(cams={"cam_val": CameraConf(1.0, (100, 200))})
+scenecfg = SceneConfig(
+    cams={
+        "cam_val": CameraConf(
+            1.0,
+            (100, 200),
+            T=PosesConf([Pose(0.1, 0.0, 0.0, 1, 2, 3), Pose(0.1, 0.0, 0.0, 4, 5, 6)]),
+        )
+    }
+)
 print(to_yaml(scenecfg))
 scene = instantiate(scenecfg)
+print(scene.cameras["cam_val"].T)
 print(list(scene.volume.rf.parameters()))
 
 torch.save(scene.state_dict(), "test.pth")
@@ -141,67 +30,3 @@ scene.load_state_dict(torch.load("test.pth"))
 
 print(scene)
 print(list(scene.volume.rf.parameters()))
-
-
-# print(to_yaml(SceneConfig))
-
-# cams = {"cam_train": Camera(1.0, (100, 200)), "cam_val": Camera(0.5, (100, 200))}
-# vol = Volume(RadianceField(), SpatialFilter(), BoxTransform((-1.0,) * 3, (1.0,) * 3))
-# scene = Scene(cams, vol)
-
-# SceneConfig = builds(scene, populate_full_signature=True)
-# print(to_yaml(SceneConfig))
-
-# # def make_poses(poses: list[dict[str, Float3]]) -> torch.Tensor:
-# #     ts = []
-# #     for p in poses:
-# #         t = torch.eye(4)
-# #         t[:3, -1] = torch.as_tensor(p["tvec"])
-# #         ts.append(t)
-# #     return torch.stack(ts, 0)
-
-
-# PosesConf = builds(make_poses, populate_full_signature=True)
-# posescfg = PosesConf(
-#     poses=[
-#         {"rvec": (0.0, 0.0, 0.0), "tvec": (1.0, 2.0, 3.0)},
-#         {"rvec": (0.0, 0.0, 0.0), "tvec": (1.0, 2.0, 3.0)},
-#         {"rvec": (0.0, 0.0, 0.0), "tvec": (1.0, 2.0, 3.0)},
-#     ]
-# )
-# print(to_yaml(posescfg))
-
-# myposes = instantiate(posescfg, _convert_="partial")
-# print(myposes)
-
-
-# Scene:
-#     cameras['train']
-#     volume.aabb
-#     volume.radiance_field
-#     volume.spatial_filter
-
-# RadianceRenderer:
-#     tsampler
-
-#     rnd(cam, poses, uv, volume)
-
-
-# # print(world)
-
-# # @hydra.main(config_path=None, config_name="my_app")
-# # def task_function(cfg: Config):
-
-# #     player = instantiate(cfg.player)  # an instance of `Character`
-
-# #     print(player)
-
-# #     with open("player_log.txt", "w") as f:
-# #         f.write("Game session log:\n")
-# #         f.write(f"Player: {player}\n")
-
-# #     return player
-
-
-# # if __name__ == "__main__":
-# #     task_function()
--- torchngp/conf.py
+++ torchngp/conf.py
@@ -1,22 +1,54 @@
 import torch
-
+import dataclasses
 from hydra_zen import make_custom_builds_fn
 
+from . import functional
 
 builds = make_custom_builds_fn(populate_full_signature=True)
 
 
 class Camera(torch.nn.Module):
-    def __init__(self, focal_length: float, size: tuple[int, int]) -> None:
+    def __init__(
+        self, focal_length: float, size: tuple[int, int], T: torch.Tensor
+    ) -> None:
         super().__init__()
         self.register_buffer("focal_length", torch.tensor([focal_length, focal_length]))
         self.register_buffer("size", torch.as_tensor(size))
+        self.register_buffer("T", torch.as_tensor(T))
 
     def extra_repr(self) -> str:
         out = ""
         out += f"focal_length={self.focal_length}, "
-        out += f"size={self.size}"
+        out += f"size={self.size}, "
+        out += f"n_poses={self.T.shape[0]}"
         return out
+
+
+@dataclasses.dataclass
+class Pose:
+    rx: float
+    ry: float
+    rz: float
+    tx: float
+    ty: float
+    tz: float
+
+
+def poses_to_tensor(poses: list[Pose]) -> torch.Tensor:
+    rvecs = torch.tensor([(p.rx, p.ry, p.rz) for p in poses]).to(torch.float64)
+    tvecs = torch.tensor([(p.tx, p.ty, p.tz) for p in poses])
+
+    theta = torch.linalg.vector_norm(rvecs, dim=-1)
+    axis = torch.zeros_like(rvecs)
+    axis[..., 0] = 1.0
+    mask = theta > 1e-7
+    axis[mask] = rvecs[mask] / theta[mask][..., None]
+    R = functional.rotation_matrix(axis, theta)
+
+    T = torch.eye(4).unsqueeze(0).expand(rvecs.shape[0], 4, 4).contiguous()
+    T[:, :3, :3] = R.float()
+    T[:, :3, 3] = tvecs
+    return T
 
 
 class RadianceField(torch.nn.Module):
@@ -61,21 +93,25 @@ class Scene(torch.nn.Module):
         self.volume = volume
 
 
-CameraConf = builds(Camera, populate_full_signature=True)
-RadianceFieldConf = builds(RadianceField, populate_full_signature=True)
-SpatialFilterConf = builds(SpatialFilter, populate_full_signature=True)
-BoxTransformConf = builds(BoxTransform, populate_full_signature=True)
+from hydra_zen import to_yaml
+
+PosesConf = builds(poses_to_tensor)
+CameraConf = builds(
+    Camera,
+    T=PosesConf([Pose(0.0, 0.0, 0.0, 0.0, 0.0, 0.0)]),
+)
+RadianceFieldConf = builds(RadianceField)
+SpatialFilterConf = builds(SpatialFilter)
+BoxTransformConf = builds(BoxTransform)
 VolConf = builds(
     Volume,
     rf=RadianceFieldConf(),
     filter=SpatialFilterConf(),
     boxt=BoxTransformConf((-1.0,) * 3, (1.0,) * 3),
-    populate_full_signature=True,
 )
 
 SceneConfig = builds(
     Scene,
-    cams={"cam_train": CameraConf(1.0, (100, 200))},
+    cams={},
     volume=VolConf(),
-    populate_full_signature=True,
 )
