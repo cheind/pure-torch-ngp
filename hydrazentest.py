178b60ffea0d8db846963f133e8f8019ca557847
Christoph Heindl <cheind@profactor.at>
2022-11-18 11:12:59 +0100

    modified hydra test

--- hydrazentest.py
+++ hydrazentest.py
@@ -4,7 +4,7 @@ from hydra_zen import instantiate, to_yaml
 
 
 from hydra_zen import save_as_yaml, load_from_yaml
-from torchngp.conf import SceneConfig, CameraConf, Scene, PosesConf, Pose
+from torchngp.conf import SceneConfig, CameraConf, Scene, PosesConf, Pose, VolumeConf
 
 scenecfg = SceneConfig(
     cams={
@@ -13,12 +13,14 @@ scenecfg = SceneConfig(
             (100, 200),
             T=PosesConf([Pose(0.1, 0.0, 0.0, 1, 2, 3), Pose(0.1, 0.0, 0.0, 4, 5, 6)]),
         )
-    }
+    },
+    volume=VolumeConf(),
 )
 print(to_yaml(scenecfg))
 scene = instantiate(scenecfg)
-print(scene.cameras["cam_val"].T)
-print(list(scene.volume.rf.parameters()))
+
+# print(scene.cameras["cam_val"].T)
+# print(list(scene.volume.rf.parameters()))
 
 torch.save(scene.state_dict(), "test.pth")
 save_as_yaml(scenecfg, "test.yaml")
@@ -28,5 +30,5 @@ scenecfg = load_from_yaml("test.yaml")
 scene: Scene = instantiate(scenecfg)
 scene.load_state_dict(torch.load("test.pth"))
 
-print(scene)
-print(list(scene.volume.rf.parameters()))
+# print(scene)
+print(list(scene.parameters()))
