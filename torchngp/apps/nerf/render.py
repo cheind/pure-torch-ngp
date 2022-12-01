295311f3ee4b5c9188788233e015bd805200304e
Christoph Heindl <cheind@profactor.at>
2022-12-01 15:49:07 +0100

    added render task

--- torchngp/apps/nerf/render.py
+++ torchngp/apps/nerf/render.py
@@ -1,10 +1,12 @@
 import logging
 import hydra
 import numpy as np
+import io
 import torch
+from pathlib import Path
 from hydra.core.config_store import ConfigStore
-from hydra_zen import instantiate, make_config, to_yaml
-from omegaconf import MISSING, DictConfig
+from hydra_zen import instantiate, make_config, load_from_yaml, ZenField as zf
+from omegaconf import MISSING, DictConfig, OmegaConf
 
 from torchngp import (
     functional,
@@ -20,20 +22,25 @@ _logger = logging.getLogger("torchngp")
 logging.getLogger("PIL").setLevel(logging.WARNING)
 
 
-NerfRenderConf = make_config(volume=MISSING, weights=MISSING)
+RenderTaskConf = make_config(
+    ckpt=zf(Path, MISSING), output_dir="${hydra:runtime.output_dir}"
+)
 cs = ConfigStore.instance()
-cs.store(name="nerf_render", node=NerfRenderConf)
+cs.store(name="render_task", node=RenderTaskConf)
 
 
 @torch.no_grad()
-@hydra.main(version_base="1.2", config_path=None, config_name="nerf")
+@hydra.main(version_base="1.2", config_path="../../../cfgs/", config_name="render_task")
 def render_task(cfg: DictConfig):
-    print(to_yaml(cfg))
+    OmegaConf.resolve(cfg)
+    _logger.info(f"Loading model from {cfg.ckpt}")
+    ckpt_data = torch.load(cfg.ckpt)
+    train_cfg = load_from_yaml(io.StringIO(ckpt_data["config"]))
 
-    vol: volumes.Volume = instantiate(cfg.volume)
-    data = torch.load(cfg.weights)
-    vol.load_state_dict(data["volume"])
-    vol.cuda()
+    dev = torch.device("cuda") if torch.cuda.is_available else torch.device("cpu")
+    vol: volumes.Volume = instantiate(train_cfg.volume)
+    vol.load_state_dict(ckpt_data["volume"])
+    vol.to(dev).eval()
     rnd = rendering.RadianceRenderer(
         tsampler=sampling.StratifiedRayStepSampler(n_samples=512)
     )
@@ -73,9 +80,10 @@ def render_task(cfg: DictConfig):
     rgba = training.render_images(
         vol, rnd, cam, None, use_amp=True, n_samples_per_view=cam.size[0]
     )
-    training.save_image("test.png", rgba)
+    _logger.info(f"Saving results to {cfg.output_dir}")
+    training.save_image(Path(cfg.output_dir) / "render.png", rgba)
 
-    # python -m torchngp.apps.nerf.render --config-path ~/dev/torch-instant-ngp/outputs/2022-12-01/09-14-17/.hydra/ --config-name config.yaml +weights="outputs/2022-12-01/09-14-17/nerf_step\=639.pth"
+    # python -m torchngp.apps.nerf.render +ckpt=/home/cheind@profactor.local/dev/torch-instant-ngp/outputs/2022-12-01_14-28-40-run/nerf_step_9215.pth
 
 
 if __name__ == "__main__":
