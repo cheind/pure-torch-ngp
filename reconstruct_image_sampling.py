2a34200c6e6119c0e7713bf455fc434cabbd4a8a
Christoph Heindl <christoph.heindl@gmail.com>
2022-12-03 21:10:34 +0100

    added demo for reconstructing images from uv samples

---
+++ reconstruct_image_sampling.py
@@ -0,0 +1,73 @@
+from torchngp import functional
+from torchngp import encoding
+from torchngp import sampling
+from torchngp import geometric
+import torch
+from itertools import islice
+
+
+def reconstruct(
+    image_rgba,
+    method=sampling.generate_random_uv_samples,
+    M=512,
+    batches=512,
+    subpixel=True,
+):
+    H, W = image_rgba.shape[-2:]
+    cam = geometric.MultiViewCamera((1, 1), (0, 0), size=(W, H), poses=[torch.eye(4)])
+    gen = method(cam, image_rgba, n_samples_per_cam=M, subpixel=subpixel)
+    rgba_rec = torch.zeros(
+        (H + 2, W + 2, 4)
+    )  # slightly larger to account for outside corners
+    rgba_w = torch.zeros((H + 2, W + 2), dtype=torch.float64)
+    for uv, rgba in islice(gen, batches):
+        rgba = rgba[0]
+        uv = uv[0]
+        corners, weights, mask = encoding._bilinear_params_2d(uv, shape=(H, W))
+        # Fast, but collisions
+        mask = mask.any(-1)
+        c = corners[mask]
+        w = weights[mask]
+        rgba = rgba[mask]
+        rgba_rec[c[..., 1] + 1, c[..., 0] + 1] += rgba[:, None, :] * w[..., None]
+        rgba_w[c[..., 1] + 1, c[..., 0] + 1] += w
+
+        # Slow but correct
+        # for c, w, m, color in zip(corners, weights, mask, rgba):
+        #     if m.all():
+        #         rgba_rec[c[:, 1]+1, c[:, 0]+1] += color[None] * w[:, None]
+        #         rgba_w[c[:, 1]+1, c[:, 0]+1] += w
+
+    mask = rgba_w > 0
+    rgba_rec[mask] /= rgba_w[mask].unsqueeze(-1)
+    rgba_rec = rgba_rec[1:-1, 1:-1]
+    return rgba_rec
+
+
+def main():
+
+    image_rgba = functional.load_image(["data/lenna.png"])
+
+    import matplotlib.pyplot as plt
+
+    cfg = dict(M=512, batches=512, subpixel=True)
+
+    torch.manual_seed(123)
+    rec1 = reconstruct(image_rgba, method=sampling.generate_randperm_uv_samples, **cfg)
+    torch.manual_seed(123)
+    rec2 = reconstruct(image_rgba, method=sampling.generate_random_uv_samples, **cfg)
+
+    gt_img = image_rgba[0].permute(1, 2, 0)
+
+    fig, axs = plt.subplots(2, 3, sharex=True, sharey=True)
+    axs[0, 0].imshow(gt_img)
+    axs[0, 1].imshow(rec1)
+    axs[0, 2].imshow((gt_img - rec1).abs().sum(-1), vmin=0, vmax=1)
+    axs[1, 0].imshow(gt_img)
+    axs[1, 1].imshow(rec2)
+    axs[1, 2].imshow((gt_img - rec2).abs().sum(-1), vmin=0, vmax=1)
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
