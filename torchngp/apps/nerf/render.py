f67bcba7a3630524b46095de697ee8dfb0dae07c
Christoph Heindl <cheind@profactor.at>
2022-12-09 13:39:22 +0100

    added depth rendering

--- torchngp/apps/nerf/render.py
+++ torchngp/apps/nerf/render.py
@@ -3,11 +3,10 @@ import hydra
 import numpy as np
 import io
 import torch
-import dataclasses
 
 from pathlib import Path
 from hydra.core.config_store import ConfigStore
-from hydra_zen import instantiate, make_config, load_from_yaml, ZenField as zf, builds
+from hydra_zen import instantiate, make_config, load_from_yaml, ZenField as zf
 from omegaconf import MISSING, DictConfig, OmegaConf
 
 from torchngp import modules
@@ -20,21 +19,16 @@ _logger = logging.getLogger("torchngp")
 logging.getLogger("PIL").setLevel(logging.WARNING)
 
 
-@dataclasses.dataclass
-class OutputOptions:
-    dir: str = "${hydra:runtime.output_dir}"
-    render_setup: bool = True
-    transparent: bool = True
-    grid: bool = False
-    fname: str = "image_{idx:03d}.png"
-
-
 SphericalPosesConf = config.build_conf(helpers.spherical_poses)
-OutputOptionsConf = builds(OutputOptions, populate_full_signature=True)
 
 RenderTaskConf = make_config(
     ckpt=zf(str, MISSING),
-    output=OutputOptionsConf(),
+    out_dir=zf(str, "${hydra:runtime.output_dir}"),
+    rgba_fname="rgba_{idx:03d}.png",
+    depth_fname="depth_{idx:03d}.png",
+    rgba_transparent=False,
+    depth_dynamic_range=False,
+    as_grid=False,
     n_rays_parallel_log2=zf(int, 14),
     poses=SphericalPosesConf(
         20,
@@ -49,6 +43,8 @@ RenderTaskConf = make_config(
         principal_point=(255.5, 255.5),
         size=(512, 512),
         poses="${poses}",
+        tnear=0,
+        tfar=10,
     ),
     renderer=modules.RadianceRendererConf(
         tsampler=modules.StratifiedRayStepSamplerConf(512)
@@ -74,24 +70,40 @@ def render_task(cfg: DictConfig):
     rnd: modules.RadianceRenderer = instantiate(cfg.renderer).to(dev)
     cam: modules.MultiViewCamera = instantiate(cfg.camera).to(dev)
 
-    ax = plotting.plot_world(vol.aabb, cam)
+    # ax = plotting.plot_world(vol.aabb, cam)
     # plt.show()
 
-    _logger.info(f"Rendering, saving results to {cfg.output.dir}")
-    rgba = rnd.trace_rgba(
+    _logger.info(f"Rendering, saving results to {cfg.out_dir}")
+    rgbad = rnd.trace(
         vol, cam, use_amp=True, n_rays_parallel=2**cfg.n_rays_parallel_log2
     )
-    if not cfg.output.transparent:
+    rgba = rgbad[:, :4]
+    depth = rgbad[:, 4:5]
+    _logger.info(
+        f"Min depth {depth.min().item():.3f}, max depth {depth.max().item():.3f}"
+    )
+    if cfg.depth_dynamic_range:
+        depth = functional.scale_depth(depth, depth.min(), depth.max())
+    else:
+        depth = functional.scale_depth(depth, cam.tnear, cam.tfar)
+
+    if not cfg.rgba_transparent:
         rgba = functional.compose_image_alpha(rgba, 1.0)
 
     functional.save_image(
         rgba,
-        str(Path(cfg.output.dir) / cfg.output.fname),
-        individual=not cfg.output.grid,
+        str(Path(cfg.out_dir) / cfg.rgba_fname),
+        individual=not cfg.as_grid,
     )
 
-    # python -m torchngp.apps.nerf.render +ckpt=/home/cheind@profactor.local/dev/torch-instant-ngp/outputs/2022-12-01/15-48-06/nerf_step_6143.pth poses.n_poses=60 output.grid=False output.transparent=False
-    # ffmpeg -i /home/cheind@profactor.local/dev/torch-instant-ngp/outputs/2022-12-02/11-44-35/image_%03d.png -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 10 output.gif
+    functional.save_image(
+        depth,
+        str(Path(cfg.out_dir) / cfg.depth_fname),
+        individual=not cfg.as_grid,
+    )
+
+    # python -m torchngp.apps.nerf.render ckpt=/home/cheind@profactor.local/dev/torch-instant-ngp/outputs/2022-12-09/10-47-28/nerf_step_4095.pth poses.n_poses=60 as_grid=False rgba_transparent=False depth_dynamic_range=True
+    # ffmpeg -i /home/cheind@profactor.local/dev/torch-instant-ngp/outputs/2022-12-02/11-44-35/rgba_%03d.png -vf "fps=10,scale=320:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 10 output.gif
 
 
 if __name__ == "__main__":
