541b109e55af60f2f83e7b9320551df6f7ac472c
Christoph Heindl <cheind@profactor.at>
2022-12-09 08:36:48 +0100

    simplified hashing

--- torchngp/modules/encoding.py
+++ torchngp/modules/encoding.py
@@ -342,16 +342,16 @@ def _bilinear_params_3d(
 
 def _hash_xor(x: torch.LongTensor, n_indices: int) -> torch.LongTensor:
     # See https://matthias-research.github.io/pages/publications/tetraederCollision.pdf
-    N = x.dim() - 1
-    Q = x.shape[-1]
-    pi = torch.tensor([1, 2654435761, 805459861], dtype=x.dtype, device=x.device)[:Q]
-    pi = pi.view(*([1] * N), Q)
 
-    xpi_int = x * pi
-    indices = xpi_int[..., 0]
-    for d in range(1, Q):
-        indices = torch.bitwise_xor(indices, xpi_int[..., d])
-    return indices % n_indices
+    dev = x.device
+    dtype = x.dtype
+    primes = [1, 2654435761, 805459861]
+
+    ids = torch.zeros(x.shape[:-1], dtype=dtype, device=dev)
+    for i in range(x.shape[-1]):
+        ids ^= x[..., i] * primes[i]
+
+    return ids % n_indices
 
 
 def _hash_ravel(x: torch.LongTensor, shape: tuple[int, ...]) -> torch.LongTensor:
